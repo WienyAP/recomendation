b31f221740d36633c6ddbd337ecb3b52a63a165c
Wieny Amalia <wienyamalia@gmail.com>
2023-06-10 19:53:04 +0800

    commit 111

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require ("express");
+const dotenv = require ("dotenv");
+const bodyParser = require ("body-parser");
+const cookieParser = require ("cookie-parser");
+const cors = require ("cors");
+const { Sequelize } = require ("sequelize");
+// import mysql = require (mysql2")
+dotenv.config();
+const app = express();
+
+//configuration database
+const db = new Sequelize('recommendation', 'root', '123', {
+    host: '34.101.227.187', //sql instance public IP
+    dialect: 'mysql'
+});
+
+try {
+    db.authenticate();
+    console.log('Database Connected...')
+} catch (error) {
+    console.log(error);    
+};
+
+// app.use(cors({ credentials: true, origin: '0.0.0.0'}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+const {DataTypes} = Sequelize;
+const Rec = db.define('recommendation', {
+    Land_name: DataTypes.STRING,
+    TypeofSoil: DataTypes.STRING,
+    soil_pH: DataTypes.STRING,
+    Soil_Organic: DataTypes.STRING,
+    Plant_Type: DataTypes.STRING
+}, {
+    freezeTableName: true
+});
+
+// // create table;
+// (async() => {
+//     await db.sync();
+// })();
+
+
+//get all of data Recommendation
+app.get('/DataRec', async(req, res) => {
+    try {
+        const rec = await Rec.findAll();
+        res.json(rec);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post('/newRec', async(req, res) => {
+    const {Land_name, TypeofSoil, soil_pH, Soil_Organic, Plant_Type} = req.body;
+    //create data into db
+    try {
+        await Rec.create({
+            Land_name: Land_name,
+            TypeofSoil: TypeofSoil,
+            soil_pH: soil_pH,
+            Soil_Organic: Soil_Organic,
+            Plant_Type: Plant_Type
+        });
+        res.json({msg: "Created Success"});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.delete('/DataRec/:id', async(req, res) => {
+    const id = req.params.id;
+
+    //delete from db
+    try {
+        await Rec.destroy({
+            where: {id: id},
+        });
+        res.json({msg: "Data was deleted"})
+    } catch (error) {
+        console.log(error);        
+    }
+});
+
+app.listen(8080, () => console.log('Server running at port 8080'));
